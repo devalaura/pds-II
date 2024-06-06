786e710631724ada945263ba10587605178d6cb2
Laura Ramos <analauraalves25@gmail.com>
2024-06-06 11:25:37 -0300

    fix(funcionario): add virtual mark to method to be override

--- heranca/hpp/Funcionario.hpp
+++ heranca/hpp/Funcionario.hpp
@@ -13,7 +13,7 @@ class Funcionario
 
     public:
         Funcionario(string nome, int id, double salarioBase = 1412.00);
-        double calcularSalarioTotal();
+        virtual double calcularSalarioTotal();
 };
 
 #endif
